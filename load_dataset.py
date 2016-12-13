1e705179eab0bb562c882b572495bafad353cf1b
nabergh <nab8kd@virginia.edu>
2016-12-13 18:51:02 -0500

    added generator for loading recipes with names

--- load_dataset.py
+++ load_dataset.py
@@ -6,6 +6,7 @@ all_recipes = pickle.load(open('dataset/cleaned_recipes.p', 'rb'))
 char2id = pickle.load(open('dataset/char2id.p', 'rb'))
 id2char = pickle.load(open('dataset/id2char.p', 'rb'))
 max_recipe_length = 500
+max_name_length = 50
 num_recipes = 30724
 
 def is_number(s):
@@ -58,3 +59,55 @@ def training_set_generator(num_recipes):
                     nextChars[i, j, char2id['E']] = 1
         
         yield (inputChars, nextChars)
+
+def name_booze_permuter(recipes):
+    while True:
+        for rec in recipes:
+            if len(rec['name']) <= max_name_length:
+                ings = rec['ingredients']
+                i = 0
+                while i < len(ings) and is_number(ings[i][0]):
+                    i += 1
+
+                ing_list = ings[:i]
+                garn_list = ings[i:]
+
+                random.shuffle(ing_list)
+                random.shuffle(garn_list)
+
+                ing_list.extend(garn_list)
+                yield ('\n'.join(ing_list), rec['name'].lower())
+
+def name_training_set_generator(num_recipes):
+    recipe_generator = name_booze_permuter(all_recipes)
+    while True:
+        recipe_list = []
+        name_list = []
+        while len(recipe_list) < num_recipes:
+            example = next(recipe_generator)
+            recipe_list.append(example[0])
+            name_list.append(example[1])
+            
+        maxSequenceLength = max_recipe_length + 1
+        maxNameSequenceLength = maxSequenceLength
+
+        inputChars = np.zeros((num_recipes, maxSequenceLength, len(char2id)), dtype=np.bool)
+        nameChars = np.zeros((num_recipes, maxNameSequenceLength, len(char2id)), dtype=np.bool)
+
+        for i in range(0, num_recipes):
+            inputChars[i, 0, char2id['S']] = 1
+            for j in range(1, maxSequenceLength):
+                if j < len(recipe_list[i]) + 1:
+                    inputChars[i, j, char2id[recipe_list[i][j - 1]]] = 1
+                else:
+                    inputChars[i, j, char2id['E']] = 1
+            nameChars[i, 0, char2id['S']] = 1
+            for j in range(1, maxNameSequenceLength):
+                if j < len(name_list[i]) + 1:
+                    if name_list[i][j - 1] not in char2id:
+                        nameChars[i, j, char2id[' ']] = 1
+                    else:
+                        nameChars[i, j, char2id[name_list[i][j - 1]]] = 1
+                else:
+                    nameChars[i, j, char2id['E']] = 1
+        yield (inputChars, nameChars)
